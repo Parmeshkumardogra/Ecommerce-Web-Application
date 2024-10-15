b687b627c85e044520d98591ff59123426e7aeb1
Parmesh2 Kumar <parmesh2.kumar@ril.com>
2024-10-15 11:39:27 +0530

    route

--- routes/routes.go
+++ routes/routes.go
@@ -5,51 +5,47 @@ import (
 
 	"github.com/BMS/controllers"
 	accountcontroller "github.com/BMS/controllers/accountController"
-	"github.com/BMS/controllers/loginController"
-
-	"github.com/BMS/controllers/redisController"
+	loginController "github.com/BMS/controllers/loginController"
+	redisController "github.com/BMS/controllers/redisController"
 	"github.com/BMS/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func SetRoutes() *gin.Engine{
-	route := gin.Default();
-	
-	route.GET("/ping",func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK,gin.H{"msg":"PONG"});
+func SetRoutes() *gin.Engine {
+	route := gin.Default()
+
+	route.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"msg": "PONG"})
 	})
 
-	route.POST("/signUp",loginController.Signup);
+	route.POST("/signUp", loginController.Signup)
 
-	route.POST("/login",loginController.LoginToSendOTP);
+	route.POST("/login", loginController.LoginToSendOTP)
 
 	//shortToken authentication
-	authenticateOTPShortToken := route.Group("/otpTokenVerify");
-	authenticateOTPShortToken.Use(middleware.AuthenticateForShortToken);
-	authenticateOTPShortToken.POST("/verifyOTP", loginController.VerifyOTP);
-
+	authenticateOTPShortToken := route.Group("/otpTokenVerify")
+	authenticateOTPShortToken.Use(middleware.AuthenticateForShortToken)
+	authenticateOTPShortToken.POST("/verifyOTP", loginController.VerifyOTP)
 
 	//long token authentication
 
-	authenticateForLongToken := route.Group("/authorised");
-	authenticateForLongToken.Use(middleware.AuthenticateForLongToken);
+	authenticateForLongToken := route.Group("/authorised")
+	authenticateForLongToken.Use(middleware.AuthenticateForLongToken)
 	// authenticateForLongToken.POST("/findOne",controllers.FindOne);
-	authenticateForLongToken.POST("/createProfile",accountcontroller.CreateProfile);
-	authenticateForLongToken.POST("/createAccount",accountcontroller.CreateSavingAccount);
+	authenticateForLongToken.POST("/createProfile", accountcontroller.CreateProfile)
+	authenticateForLongToken.POST("/createAccount", accountcontroller.CreateSavingAccount)
 
 	//db intereaction servcies
 
-	
-	route.POST("/insertMany",controllers.MultipleUser);
-	
-	route.POST("/findMany",controllers.FindManyUsers);
+	route.POST("/insertMany", controllers.MultipleUser)
 
-	route.POST("/findOne",controllers.FindOne);
+	route.POST("/findMany", controllers.FindManyUsers)
+
+	route.POST("/findOne", controllers.FindOne)
 	//temproary services
-	route.POST("/getOTPfromRedis",redisController.FetchOTPFromRedis);
+	route.POST("/getOTPfromRedis", redisController.FetchOTPFromRedis)
 
-	route.POST("/verifyUserStatus",controllers.UserVerification);
+	route.POST("/verifyUserStatus", controllers.UserVerification)
 
-	return route;
+	return route
 }
-
